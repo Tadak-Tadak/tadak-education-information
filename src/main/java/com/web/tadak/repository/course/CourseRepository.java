568bce281ae4ea70312f2e8458564875b0e8a2d8
SXNGJUNHX <156043182+SXNGJUNHX@users.noreply.github.com>
2024-06-10 17:32:05 +0900

    [add] Course, Certification Repository 작성
    Body: Course, CertificaionRepository 클래스 생성 및 JpaRepository extends
    
    Why: Test 하기 위해서
    
    Effect: Test 코드 동작

---
+++ src/main/java/com/web/tadak/repository/course/CourseRepository.java
@@ -0,0 +1,8 @@
+package com.web.tadak.repository.course;
+
+import com.web.tadak.entity.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
