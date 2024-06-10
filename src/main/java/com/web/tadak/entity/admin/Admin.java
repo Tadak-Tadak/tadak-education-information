64638a44b5631a50b6b5e016b85da0c0dd84530a
juniel1299 <youngs5440@gmail.com>
2024-06-10 09:22:00 +0900

    [Add] 관리자 Admin에 대한 service, repository 코드 작성
    Body: 실제 데이터 베이스와 연결을 위한 repository , service 코드 추가

--- src/main/java/com/web/tadak/entity/admin/Admin.java
+++ src/main/java/com/web/tadak/entity/admin/Admin.java
@@ -2,12 +2,13 @@ package com.web.tadak.entity.admin;
 
 import jakarta.persistence.*;
 import lombok.*;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @ToString
-@Builder
+@Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class Admin {
@@ -20,24 +21,22 @@ public class Admin {
     private String adminId; // 관리자 아이디
 
     @Column(nullable = false, length = 20)
-    private String adminPw; //관리자 비밀번호
+    private String adminPw; // 관리자 비밀번호
 
     @Column(nullable = false, length = 10)
-    private String name; //관리자 이름
+    private String name; // 관리자 이름
 
     @Column(nullable = false)
-    private LocalDateTime createdAt; //관리자 계정 생성 일자
+    private LocalDateTime createdAt; // 관리자 계정 생성 일자
 
     @Column(nullable = false, length = 30)
-    private String tel; //관리자 전화번호
+    private String tel; // 관리자 전화번호
 
     @Column(nullable = false, length = 40)
-    private String email; //관리자 이메일
+    private String email; // 관리자 이메일
 
     @Column(nullable = false, length = 300)
     private String imgUrl = "default-image-url"; // 기본 이미지 URL 설정
 
-//기본 이미지 설정 보류
-
-
+    // 기본 이미지 설정 보류
 }
---
+++ src/main/java/com/web/tadak/repository/admin/AdminRepository.java
@@ -0,0 +1,12 @@
+package com.web.tadak.repository.admin;
+
+import com.web.tadak.entity.admin.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByAdminId(String adminId); // 관리자의 ID로 검색하는 메서드 추가
+}
